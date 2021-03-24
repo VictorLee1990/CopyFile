6bc1ba9cf55ae4052d5659f5dc774cef67e8da16
VictorLee1990 <victor104843@gmail.com>
2021-03-24 15:01:06 +0800

    first release
    first release

---
+++ copyFile1/copyFile1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace copyFile1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Source_Btn = new System.Windows.Forms.Button();
+            this.Dest_Btn = new System.Windows.Forms.Button();
+            this.CopyRun_Btn = new System.Windows.Forms.Button();
+            this.SourceFile_TB = new System.Windows.Forms.TextBox();
+            this.DestFile_TB = new System.Windows.Forms.TextBox();
+            this.CopyFailFile_TB = new System.Windows.Forms.TextBox();
+            this.FindInLog_Btn = new System.Windows.Forms.Button();
+            this.FindInResult_Btn = new System.Windows.Forms.Button();
+            this.LogFile_TB = new System.Windows.Forms.TextBox();
+            this.ResultFile_TB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Source_Btn
+            // 
+            this.Source_Btn.Location = new System.Drawing.Point(322, 12);
+            this.Source_Btn.Name = "Source_Btn";
+            this.Source_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Source_Btn.TabIndex = 0;
+            this.Source_Btn.Text = "Source";
+            this.Source_Btn.UseVisualStyleBackColor = true;
+            this.Source_Btn.Click += new System.EventHandler(this.Source_Btn_Click);
+            // 
+            // Dest_Btn
+            // 
+            this.Dest_Btn.Location = new System.Drawing.Point(322, 39);
+            this.Dest_Btn.Name = "Dest_Btn";
+            this.Dest_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Dest_Btn.TabIndex = 1;
+            this.Dest_Btn.Text = "Dest";
+            this.Dest_Btn.UseVisualStyleBackColor = true;
+            this.Dest_Btn.Click += new System.EventHandler(this.Dest_Btn_Click);
+            // 
+            // CopyRun_Btn
+            // 
+            this.CopyRun_Btn.Location = new System.Drawing.Point(12, 124);
+            this.CopyRun_Btn.Name = "CopyRun_Btn";
+            this.CopyRun_Btn.Size = new System.Drawing.Size(385, 23);
+            this.CopyRun_Btn.TabIndex = 2;
+            this.CopyRun_Btn.Text = "Run";
+            this.CopyRun_Btn.UseVisualStyleBackColor = true;
+            this.CopyRun_Btn.Click += new System.EventHandler(this.CopyRun_Btn_Click);
+            // 
+            // SourceFile_TB
+            // 
+            this.SourceFile_TB.Location = new System.Drawing.Point(12, 12);
+            this.SourceFile_TB.Name = "SourceFile_TB";
+            this.SourceFile_TB.ReadOnly = true;
+            this.SourceFile_TB.Size = new System.Drawing.Size(304, 22);
+            this.SourceFile_TB.TabIndex = 3;
+            // 
+            // DestFile_TB
+            // 
+            this.DestFile_TB.Location = new System.Drawing.Point(12, 40);
+            this.DestFile_TB.Name = "DestFile_TB";
+            this.DestFile_TB.ReadOnly = true;
+            this.DestFile_TB.Size = new System.Drawing.Size(304, 22);
+            this.DestFile_TB.TabIndex = 4;
+            // 
+            // CopyFailFile_TB
+            // 
+            this.CopyFailFile_TB.Location = new System.Drawing.Point(12, 153);
+            this.CopyFailFile_TB.Multiline = true;
+            this.CopyFailFile_TB.Name = "CopyFailFile_TB";
+            this.CopyFailFile_TB.ReadOnly = true;
+            this.CopyFailFile_TB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.CopyFailFile_TB.Size = new System.Drawing.Size(384, 237);
+            this.CopyFailFile_TB.TabIndex = 5;
+            // 
+            // FindInLog_Btn
+            // 
+            this.FindInLog_Btn.Location = new System.Drawing.Point(322, 67);
+            this.FindInLog_Btn.Name = "FindInLog_Btn";
+            this.FindInLog_Btn.Size = new System.Drawing.Size(75, 23);
+            this.FindInLog_Btn.TabIndex = 6;
+            this.FindInLog_Btn.Text = "FindLog";
+            this.FindInLog_Btn.UseVisualStyleBackColor = true;
+            this.FindInLog_Btn.Click += new System.EventHandler(this.FindInLog_Btn_Click);
+            // 
+            // FindInResult_Btn
+            // 
+            this.FindInResult_Btn.Location = new System.Drawing.Point(322, 95);
+            this.FindInResult_Btn.Name = "FindInResult_Btn";
+            this.FindInResult_Btn.Size = new System.Drawing.Size(75, 23);
+            this.FindInResult_Btn.TabIndex = 7;
+            this.FindInResult_Btn.Text = "FindResult";
+            this.FindInResult_Btn.UseVisualStyleBackColor = true;
+            this.FindInResult_Btn.Click += new System.EventHandler(this.FindInResult_Btn_Click);
+            // 
+            // LogFile_TB
+            // 
+            this.LogFile_TB.Location = new System.Drawing.Point(12, 68);
+            this.LogFile_TB.Name = "LogFile_TB";
+            this.LogFile_TB.ReadOnly = true;
+            this.LogFile_TB.Size = new System.Drawing.Size(304, 22);
+            this.LogFile_TB.TabIndex = 8;
+            // 
+            // ResultFile_TB
+            // 
+            this.ResultFile_TB.Location = new System.Drawing.Point(12, 96);
+            this.ResultFile_TB.Name = "ResultFile_TB";
+            this.ResultFile_TB.ReadOnly = true;
+            this.ResultFile_TB.Size = new System.Drawing.Size(304, 22);
+            this.ResultFile_TB.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(444, 403);
+            this.Controls.Add(this.ResultFile_TB);
+            this.Controls.Add(this.LogFile_TB);
+            this.Controls.Add(this.FindInResult_Btn);
+            this.Controls.Add(this.FindInLog_Btn);
+            this.Controls.Add(this.CopyFailFile_TB);
+            this.Controls.Add(this.DestFile_TB);
+            this.Controls.Add(this.SourceFile_TB);
+            this.Controls.Add(this.CopyRun_Btn);
+            this.Controls.Add(this.Dest_Btn);
+            this.Controls.Add(this.Source_Btn);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CopyFile";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Source_Btn;
+        private System.Windows.Forms.Button Dest_Btn;
+        private System.Windows.Forms.Button CopyRun_Btn;
+        private System.Windows.Forms.TextBox SourceFile_TB;
+        private System.Windows.Forms.TextBox DestFile_TB;
+        private System.Windows.Forms.TextBox CopyFailFile_TB;
+        private System.Windows.Forms.Button FindInLog_Btn;
+        private System.Windows.Forms.Button FindInResult_Btn;
+        private System.Windows.Forms.TextBox LogFile_TB;
+        private System.Windows.Forms.TextBox ResultFile_TB;
+    }
+}
+
---
+++ copyFile1/copyFile1/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Threading;
+namespace copyFile1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        string[] array = new string[2000];
+        int Counter = 0;
+        string Path;
+        string LogPath;
+        string ResultPath;
+        private delegate void ShowFile(string i);
+        private delegate void MouseCursor(bool busy);
+    
+
+        private void Source_Btn_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"C:\Users\Desktop"; //設定初始路徑
+                                                        //ofd.Filter = "Excel檔案(*.xls)|*.xls|Csv檔案(*.csv)|*.csv|所有檔案(*.*)|*.*"; //設定“另存為檔案型別”或“檔案型別”框中出現的選擇內容
+            ofd.Filter = "文字檔 (*.txt)|*.txt";
+            ofd.FilterIndex = 1; //設定預設顯示檔案型別為Csv檔案(*.csv)|*.csv
+            ofd.Title = "開啟檔案"; //獲取或設定檔案對話方塊標題
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(ofd.FileName);
+                SourceFile_TB.Text = (ofd.FileName);
+                while ((array[Counter] = file.ReadLine()) != null)
+                {
+
+                    //Console.Write(array[i] + "\n");
+
+                    Counter++;
+                }
+            }
+
+        }
+
+        private void Dest_Btn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Path = new FolderBrowserDialog();
+            Path.ShowDialog();
+            Console.Write(Path.SelectedPath + "\n");
+            this.Path = Path.SelectedPath;
+            DestFile_TB.Text = this.Path;
+        }
+
+        private void CopyRun_Btn_Click(object sender, EventArgs e)
+        {
+            
+            Thread t = new Thread(RunCopyWork);
+            t.Start();
+          
+
+        }
+
+        private void RunCopyWork()
+        {
+            if (SourceFile_TB.Text == "" || DestFile_TB.Text == "" || LogFile_TB.Text == "" || ResultFile_TB.Text == "")
+            {
+                MessageBox.Show("folder error");
+                return;
+            }
+               
+            bool cursor = true;
+            MouseCursor MC = new MouseCursor(MouseCursorInit);
+            this.Invoke(MC,cursor);
+             
+            DirectoryInfo dir = new DirectoryInfo(Path + "\\");
+            DirectoryInfo logDir = new DirectoryInfo(LogPath + "\\");
+            DirectoryInfo resultDir = new DirectoryInfo(ResultPath + "\\");
+            Directory.GetParent(Path);
+            int flag;
+            if (Directory.Exists(Directory.GetParent(Path)  + "\\temp"))
+            {
+                //資料夾存在
+            }
+            else
+            {
+                //新增資料夾
+                Directory.CreateDirectory(Directory.GetParent(Path) + "\\temp");
+                Directory.CreateDirectory(Directory.GetParent(Path) + "\\temp\\" + dir.Name);
+                Directory.CreateDirectory(Directory.GetParent(Path) + "\\temp\\" + logDir.Name);
+                Directory.CreateDirectory(Directory.GetParent(Path) + "\\temp\\" + resultDir.Name);
+            }
+            for (int i = 0; i < 2000; i++)
+            {
+                if (array[i] == null)
+                {
+                    cursor = false;
+                    this.Invoke(MC, cursor);
+
+                    if (CopyFailFile_TB.Text == "")
+                    {
+                       
+                        MessageBox.Show("Copy Complete !");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Copy Fail !");
+                    }
+
+                    return;
+                }
+                flag = 0;
+                //  foreach (DirectoryInfo dChild in dir.GetDirectories(array[i]+"*"))
+                foreach (FileInfo f in dir.GetFiles("*.txt"))
+                {//如果用GetDirectories("ab*"),那麼全部以ab開頭的目錄會被顯示
+
+                    Regex regex = new Regex(@array[i]);//查詢檔案名稱中有關鍵字的文件
+                    Match m = regex.Match(f.ToString());
+                    if (m.Success == true)
+                    {
+                        // Console.Write(f.Name + "<BR>");//列印目錄名
+                        File.Copy(f.FullName, (Directory.GetParent(Path) + "\\temp\\" + dir.Name + "\\" + f.Name), true);
+
+                        flag++; 
+                    }
+
+                    // Response.Write(dChild.FullName + "<BR>");//列印路徑和目錄名
+                }
+
+                if (flag < 2)
+                {
+                    ShowFile Sf = new ShowFile(ShowText);
+                    this.Invoke(Sf, "NO Control_Log: " + array[i]);
+
+                }
+
+                bool haveWord = false;
+                FindInDirectory(LogPath, array[i],ref haveWord, logDir.Name);
+                if (!haveWord)
+                {
+                    ShowFile Sf = new ShowFile(ShowText);
+                    this.Invoke(Sf,"NO LOG: " + array[i]);
+                }
+
+                haveWord = false;
+                FindInDirectory(ResultPath, array[i], ref haveWord, resultDir.Name);
+                if (!haveWord)
+                {
+                    ShowFile Sf = new ShowFile(ShowText);
+                    this.Invoke(Sf, "NO Result: " + array[i]);
+                }
+            }
+
+          
+        }
+
+        private void ShowText(string file)
+        {
+            CopyFailFile_TB.Text += file.ToString() + "\r\n";
+        }
+
+        private void MouseCursorInit(bool flag)
+        {
+            if (flag)
+            {
+                this.Cursor = Cursors.WaitCursor;//漏斗指標
+                Source_Btn.Enabled = false;
+                Dest_Btn.Enabled = false;
+                CopyRun_Btn.Enabled = false;
+                FindInLog_Btn.Enabled = false;
+                FindInResult_Btn.Enabled = false;
+            }
+            else
+            {
+                this.Cursor = Cursors.Default;//還原預設
+                Source_Btn.Enabled = true;
+                Dest_Btn.Enabled = true;
+                CopyRun_Btn.Enabled = true;
+                FindInLog_Btn.Enabled = true;
+                FindInResult_Btn.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 從檔案中找關鍵字
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="word"></param>
+        private bool FindInFile(string filename, string word)
+        {
+            System.IO.StreamReader sr = System.IO.File.OpenText(filename);
+            string s = sr.ReadToEnd();
+            sr.Close();
+            string[] temp = s.Split('\n');
+            
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i].IndexOf(word) != -1)
+                {
+                    Console.WriteLine(string.Format(
+                    "Found in: {0}\n{1}\nLine: {2} \n",
+                    filename, temp[i].Trim(), i + 1));
+                  
+                    return true;
+                  
+                }
+
+                
+            }
+            return false;
+
+        }
+
+        /// <summary>
+        /// 從資料夾中找關鍵字
+        /// </summary>
+        /// <param name="foldername"></param>
+        /// <param name="word"></param>
+        private void FindInDirectory(string foldername, string word, ref bool haveWord, string creatFoldName)
+        {
+            
+            System.IO.DirectoryInfo dif = new System.IO.DirectoryInfo(foldername);
+          
+            //遍歷資料夾中的各子資料夾
+            foreach (System.IO.DirectoryInfo di in dif.GetDirectories())
+            {
+                FindInDirectory(di.FullName, word, ref haveWord, creatFoldName);
+            }
+            //查詢資料夾中的各個檔案
+            foreach (System.IO.FileInfo f in dif.GetFiles())
+            {
+                bool haveFileFlag;
+                haveFileFlag = FindInFile(f.FullName, word);
+                haveWord |= haveFileFlag;
+                if (haveFileFlag)
+                {
+                    Directory.CreateDirectory(Directory.GetParent(Path) + "\\temp\\" + creatFoldName + "\\" + dif.Name);
+                    File.Copy(f.FullName, (Directory.GetParent(Path) + "\\temp\\" + creatFoldName + "\\" + dif.Name + "\\" + f.Name), true);
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FindInLog_Btn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Path = new FolderBrowserDialog();
+            Path.ShowDialog();
+            Console.Write(Path.SelectedPath + "\n");
+            LogPath = Path.SelectedPath;
+            LogFile_TB.Text = LogPath;
+        }
+
+        private void FindInResult_Btn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Path = new FolderBrowserDialog();
+            Path.ShowDialog();
+            Console.Write(Path.SelectedPath + "\n");
+            ResultPath = Path.SelectedPath;
+            ResultFile_TB.Text = ResultPath;
+        }
+    }
+}
